40c38457edf9384b3bc2822cad77c84a281a3714
sakatosi0728 <sakatosi0728@gmail.com>
2020-10-12 13:14:07 +0900

    ルームの保存

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,20 @@
 class RoomsController < ApplicationController
+  
   def index
     @rooms = Room.all
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    Room.create(room_params)
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, :image, :text)
+  end
+
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,3 @@
 class Room < ApplicationRecord
+  validates :text, presence: true
 end
