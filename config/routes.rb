6f3c085b8828c19088aee8ce7710a4c99c4211be
sakatosi0728 <sakatosi0728@gmail.com>
2020-10-12 15:42:17 +0900

    gem rails_12factorの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: 'rooms#index'
   resources :rooms do
     resources :messages, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
